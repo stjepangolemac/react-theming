17cee5e619b1bc111c4faa0c4a725fcaa6c5c605
Stjepan Golemac <stjepan.golemac@gmail.com>
2017-09-25 16:58:29 +0200

    Added types

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import ThemeProvider, { themeProviderDecorator } from './theme-provider'
 import withTheme from './with-theme'
 
+export default {
+  ThemeProvider,
+  withTheme,
+  themeProviderDecorator
+}
+
 export {
   ThemeProvider,
   withTheme,
