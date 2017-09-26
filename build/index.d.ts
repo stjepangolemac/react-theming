f6b1233437f75bd0d076bf310d92e788448be43b
Stjepan Golemac <stjepan.golemac@gmail.com>
2017-09-26 10:53:43 +0200

    Add reference link

---
+++ build/index.d.ts
@@ -0,0 +1,5 @@
+declare class ThemeProvider { }
+
+declare function withTheme (Component: any): any
+
+declare function themeProviderDecorator (theme: any): (story: any) => any
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/// <reference path="index.d.ts"" />
+
 import ThemeProvider, { themeProviderDecorator } from './theme-provider'
 import withTheme from './with-theme'
 
